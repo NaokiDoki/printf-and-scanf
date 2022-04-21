08ac263e6679a436507f7a698986ec1e44982aef
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-04-21 07:39:27 +0545

    Update demonstration of printf and scanf.c

--- demonstration of printf and scanf.c
+++ demonstration of printf and scanf.c
@@ -6,6 +6,7 @@ int main(){
 	double dp = 789.123456;
 	char c = 'M';
 	char str[50] = "Good night";
+	printf("enter a integer");
 	scanf("%d", &n);
 	printf("Integer value is %d",n);
 	printf("\nFloat value is %f",fp);
