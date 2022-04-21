e6dc2271654b876bd1fa234c76794da421856df6
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-04-21 07:32:34 +0545

    Add files via upload

---
+++ scanf basic data types.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int n = 11;
+	float fp = 789.123456;
+	double dp = 789.123456;
+	char c = 'M';
+	char str[50] = "Good night";
+	printf("Integer value is %d",n);
+	printf("\nFloat value is %f",fp);
+	printf("\nDouble value is %lf",dp);
+	printf("\nCharacter is %c",c);
+	printf("\nString is %s",str);
+	printf("\nOctal value is %o",n);
+	printf("\nHexadecimal value is %x",n);
+	return 0;
+}
